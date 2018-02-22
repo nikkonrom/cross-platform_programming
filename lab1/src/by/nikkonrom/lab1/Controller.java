ac974cc93ec01237ebc4d53a7072fc19f4ee27e5
Никита Романюк <nikkonrom@gmail.com>
2018-02-22 04:23:45 +0300

    Writing simple logging form

---
+++ lab1/src/by/nikkonrom/lab1/Controller.java
@@ -0,0 +1,10 @@
+package by.nikkonrom.lab1;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void adminButtonAction(ActionEvent actionEvent) {
+        
+    }
+}
--- lab1/src/by/nikkonrom/lab1/Main.java
+++ lab1/src/by/nikkonrom/lab1/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package by.nikkonrom.lab1;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
